04c375ebec97f9bf2a625bc82c6ef75d293a03e1
MatteoCarmignani <matteo.carmignani01@gmail.com>
2025-09-04 14:51:09 +0200

    completamento

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
 const myBottom = document.getElementById("accensione");
 console.log(myBottom)
 
-const myImg1 = 
+const myImg1 = document.getElementById("miaImmagine1")
 console.log(myImg1)
 
 const myImg2 = document.querySelector(".miaImmagine2");
 
-myBottom.addEventListener("click", 
-    onOff
-);
+myBottom.addEventListener('click',function() {
+  myImg1.src = 'img/yellow_lamp.png';
+});
 
-
-function onOff() {
-
-}
