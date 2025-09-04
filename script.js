ae159502fb5a5329bdf53626c48fce498a9626d4
MatteoCarmignani <matteo.carmignani01@gmail.com>
2025-09-04 14:30:37 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const myBottom = document.getElementById("accensione");
+console.log(myBottom)
+
+const myImg1 = 
+console.log(myImg1)
+
+const myImg2 = document.querySelector(".miaImmagine2");
+
+myBottom.addEventListener("click", 
+    onOff
+);
+
+
+function onOff() {
+
+}
